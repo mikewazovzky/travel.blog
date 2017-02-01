12a53227e6a5ad54685e9c23b97f5619b333aff5
Mike Wazovzky <mike.wazovzky@gmail.com>
2017-02-01 23:00:18 +0300

    Blog layout structure

--- resources/views/layouts/blog/sidebar.blade.php
+++ resources/views/layouts/blog/sidebar.blade.php
@@ -1,28 +1,44 @@
-  <div class="sidebar-module sidebar-module-inset">
-    <h4>About</h4>
-    <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-  </div>
-  
-  <div class="sidebar-module">
-    <h4>Archives</h4>
-    <ol class="list-unstyled">
+<div class="panel panel-default">
+    <div class="panel-body"> 
+        <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+        </div>
+    </div>
+</div>  
 
-    </ol>
-  </div>
-  
-  <div class="sidebar-module">
-    <h4>Tags</h4>
-    <ol class="list-unstyled">
+<div class="panel panel-default">
+    <div class="panel-body"> 
+        <div class="sidebar-module">
+            <h4>Archives</h4>
+            <ol class="list-unstyled">
 
-    </ol>
-  </div>  
+            </ol>
+        </div>
+    </div>
+</div>   
+
+<div class="panel panel-default">
+    <div class="panel-body"> 
+        <div class="sidebar-module">
+            <h4>Tags</h4>
+            <ol class="list-unstyled">
+
+            </ol>
+        </div>  
+    </div>
+</div>    
   
   
-  <div class="sidebar-module">
-    <h4>Elsewhere</h4>
-    <ol class="list-unstyled">
-      <li><a href="#">GitHub</a></li>
-      <li><a href="#">Twitter</a></li>
-      <li><a href="#">Facebook</a></li>
-    </ol>
-  </div>
+<div class="panel panel-default">
+    <div class="panel-body">
+        <div class="sidebar-module">
+            <h4>Elsewhere</h4>
+            <ol class="list-unstyled">
+                <li><a href="#">GitHub</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Facebook</a></li>
+            </ol>
+        </div>
+    </div>
+</div>
