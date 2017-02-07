6cabbba9dffb41189d9201a6c0ecd9defb7a3ad7
Mike <mike.wazovzky@gmail.com>
2017-02-07 17:18:48 +0300

    Sidebar links

--- resources/views/layouts/blog/sidebar.blade.php
+++ resources/views/layouts/blog/sidebar.blade.php
@@ -53,7 +53,15 @@
     <div class="panel-body"> 
         <div class="sidebar-module sidebar-module-inset">
             <h4>About</h4>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <p>
+				&copy;  <a href="http://m-lab.xyz">Monster Lab.</a>, 2017, <br> 
+				<a href="/about">О нас</a>, 
+				<a href="#">Помощь</a>, 
+				<a href="#">Условия</a>, 
+				<a href="#">Конфиденциальность</a>,
+				<a href="/contacts">Реклама</a>,
+				<a href="/contacts">Контакты</a>,
+			</p>
         </div>
     </div>
 </div>  
