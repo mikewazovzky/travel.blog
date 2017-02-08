0ef8eb2cb9f13502e3324286e71c6517e539bebb
Mike <mike.wazovzky@gmail.com>
2017-02-08 22:59:10 +0300

    News controller level auth

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,7 +7,11 @@ use App\Article;
 
 class ArticlesController extends Controller
 {
-    /**
+    public function __construct()
+	{
+		$this->middleware('auth')->except(['index', 'show']);
+	}
+	/**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
